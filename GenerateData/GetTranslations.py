224107846aeecb6978660524f037da302d7ab01e
ElLorans <lorenzo.cerreta@libero.it>
2022-12-30 17:20:16 +0100

    Remove types as they are in other file

--- GenerateData/GetTranslations.py
+++ GenerateData/GetTranslations.py
@@ -17,27 +17,14 @@ def clean_str(stringa: str) -> str:
     return stringa
 
 
-card_types = dict()
+# card_types = dict()
 
-errors = list()  # no italian name provided
+# errors = list()  # no italian name provided
 translations = dict()
 # this will take time (384418 it [11:20, 564.85it/s] for 1.5 GB)
 with open(os.path.join(SCRYFALL_FOLDER, "all-cards.json"), "rb") as j:
     for dictionary in tqdm(ijson.items(j, "item")):
-        if dictionary["lang"] == "en":
-            lower_name = dictionary["name"].lower()
-            # if card has 2 sides
-            if "card_faces" in dictionary:
-                # card type is first type
-                card_types[lower_name] = clean_str(dictionary["card_faces"][0]["type_line"])
-                for subdict in dictionary["card_faces"]:
-                    side_name = subdict["name"].lower()
-                    # some double faced tokens don't have type line for second face
-                    if "type_line" in subdict:
-                        card_types[side_name] = clean_str(subdict["type_line"])
-            elif dictionary["type_line"] != "Card":
-                card_types[lower_name] = dictionary["type_line"]
-        elif dictionary["lang"] == "it":
+        if dictionary["lang"] == "it":
             try:
                 translations[dictionary["printed_name"].lower()] = dictionary[
                     "name"
@@ -58,11 +45,11 @@ with open(os.path.join(SCRYFALL_FOLDER, "all-cards.json"), "rb") as j:
                             break
                     ita_name = " // ".join(ita_name)
                     translations[ita_name] = dictionary["name"].lower()
-                else:
-                    errors.append(dictionary["name"])
+                # else:
+                #     errors.append(dictionary["name"])
 
-errors = set(errors)
-print(f"{len(errors)} errors:\n{errors}")
+# errors = set(errors)
+# print(f"{len(errors)} errors:\n{errors}")
 
 corrections = {
     "prismari command": "Instant",
@@ -71,21 +58,6 @@ corrections = {
     "ephemerate": "Instant",
 }
 
-for k, v in corrections.items():
-    if card_types[k] != v:
-        # card_types[k] = v
-        print(k, "is", card_types[k], "should be", v)
-
-try:
-    del card_types["lands"]
-except KeyError:
-    print("No lands")
-
-print(card_types["liliana of the veil"])
-# print(card_types['liliana del velo'])
-print(card_types["lorehold command"])
-print(card_types["clearwater pathway"])
-print(card_types["find // finality"])
 print(translations["trovare // troncare"])
 
 # Save Data
