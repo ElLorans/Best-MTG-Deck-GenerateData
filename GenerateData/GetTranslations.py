2bfd4e7596b3a67e00b12afef654acfde2d00f06
ElLorans <lorenzo.cerreta@gmail.com>
2025-03-08 19:59:53 +0100

    Aetherdrift

--- GenerateData/GetTranslations.py
+++ GenerateData/GetTranslations.py
@@ -4,7 +4,8 @@ Save all-cards.json from https://scryfall.com/docs/api/bulk-data
 import os
 import time
 
-import orjson
+from msgspec import Struct
+from msgspec.json import decode
 from tqdm.auto import tqdm
 
 from utils import NEW_DATA_FOLDER, SCRYFALL_FOLDER, create_directories
@@ -18,36 +19,45 @@ def clean_str(stringa: str) -> str:
     return stringa
 
 
+class CardFaces(Struct):
+    printed_name: str | None = None
+
+
+class Card(Struct):
+    lang: str
+    name: str
+    printed_name: str | None = None
+    card_faces: list[CardFaces] | None = None
+
 translations = dict()
 
-print("Reading file... It will take up to 30 minutes")
+print("Reading file")
 start = time.time()
 with open(os.path.join(SCRYFALL_FOLDER, "all-cards.json"), "rb") as f:
-    data = orjson.loads(f.read())
+    # data = orjson.loads(f.read())
+    data: list[Card] = decode(f.read(), type=list[Card])
 print("Database loaded in", time.time() - start)
 
 for dictionary in tqdm(data):
-    if dictionary["lang"] == "it":
+    if dictionary.lang == "it":
         try:
-            translations[dictionary["printed_name"].lower()] = dictionary[
-                    "name"
-                ].lower()
-        except KeyError:
+            translations[dictionary.printed_name.lower()] = dictionary.name.lower()
+        except AttributeError:
             ita_name = list()
             # split cards don't have "printed_name" but have "card_faces"
-            if "card_faces" in dictionary:
-                for subdict in dictionary["card_faces"]:
+            if dictionary.card_faces:
+                for subdict in dictionary.card_faces:
                     try:
-                        ita_name.append(subdict["printed_name"].lower())
+                        ita_name.append(subdict.printed_name.lower())
                         # old double cards have printed_name only for first half
                         # (e.g.: Domanda/Offerta)
-                        if "/" in subdict["printed_name"]:
+                        if "/" in subdict.printed_name:
                             break
                     # Kamigawa split cards don't have printed_name for second face
-                    except KeyError:
+                    except AttributeError:
                         break
                 ita_name = " // ".join(ita_name)
-                translations[ita_name] = dictionary["name"].lower()
+                translations[ita_name] = dictionary.name.lower()
 
 
 try:
